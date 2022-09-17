d9d8f834594c5a458422b6f67a1b8b60ae0157dc
syntheit <daniel@matv.io>
2022-09-17 00:23:07 -0400

    Updated nav links

---
+++ constants/links.ts
@@ -0,0 +1,13 @@
+export const links: {
+  label: string;
+  url: string;
+  icon: string;
+  hoverColor: string;
+}[] = [
+  {
+    label: "Github",
+    url: "https://www.github.com/syntheit",
+    icon: "github.svg",
+    hoverColor: "#f5f5f5",
+  },
+];
