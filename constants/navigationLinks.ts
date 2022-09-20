3e5636a87237324df6c1cbca64407f5a37d2343b
syntheit <daniel@matv.io>
2022-09-19 21:45:05 -0400

    Added resume

--- constants/navigationLinks.ts
+++ constants/navigationLinks.ts
@@ -2,6 +2,6 @@ export const navLinks = [
   { name: "home", url: "/", color: "orange", noHomepageDisplay: true },
   { name: "about", url: "/about", color: "green" },
   { name: "portfolio", url: "/portfolio", color: "purple" },
-  { name: "resumé", url: "/resume", color: "blue" },
+  { name: "resume", url: "/Daniel\ Miller\ Resume.pdf", color: "blue" },
   { name: "contact", url: "/contact", color: "red" },
 ];
