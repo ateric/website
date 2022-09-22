4f173f96f71e3f5854b42b0a2e2f386de610344f
syntheit <daniel@matv.io>
2022-09-22 00:48:27 -0400

    Added tools constants file

---
+++ constants/tools.ts
@@ -0,0 +1,118 @@
+const icons_dir: string = "/skill_icons";
+
+export const tools: {
+  label: string;
+  icon: string;
+  color: string;
+}[] = [
+  {
+    label: "Typescript",
+    icon: `${icons_dir}/typescript.svg`,
+    color: "#007acc",
+  },
+  {
+    label: "Javascript",
+    icon: `${icons_dir}/javascript.svg`,
+    color: "#f7cd18",
+  },
+  {
+    label: "React",
+    icon: `${icons_dir}/react.svg`,
+    color: "#61dafb",
+  },
+  {
+    label: "Angular",
+    icon: `${icons_dir}/angular.svg`,
+    color: "#c3002f",
+  },
+  {
+    label: "Docker",
+    icon: `${icons_dir}/docker.svg`,
+    color: "#1f97ee",
+  },
+  {
+    label: "Tailwind",
+    icon: `${icons_dir}/tailwind.svg`,
+    color: "#07b6d5",
+  },
+  {
+    label: "Next.js",
+    icon: `${icons_dir}/nextjs.svg`,
+    color: "#0070f3",
+  },
+  {
+    label: "Sass",
+    icon: `${icons_dir}/sass.svg`,
+    color: "#bf4080",
+  },
+  {
+    label: "Node.js",
+    icon: `${icons_dir}/nodejs.svg`,
+    color: "#6da75d",
+  },
+  {
+    label: "Gatsby",
+    icon: `${icons_dir}/gatsby.svg`,
+    color: "#663399",
+  },
+  {
+    label: "PrimeNG",
+    icon: `${icons_dir}/primeng.svg`,
+    color: "#c3002f",
+  },
+  {
+    label: "Express",
+    icon: `${icons_dir}/express.svg`,
+    color: "#4baeff",
+  },
+  {
+    label: "styled-components",
+    icon: `${icons_dir}/styled-components.svg`,
+    color: "#c25094",
+  },
+  {
+    label: "Firebase",
+    icon: `${icons_dir}/firebase.svg`,
+    color: "#ffa000",
+  },
+  {
+    label: "Algolia",
+    icon: `${icons_dir}/algolia.svg`,
+    color: "#2280ef",
+  },
+  {
+    label: "Java",
+    icon: `${icons_dir}/java.svg`,
+    color: "#f58219",
+  },
+  {
+    label: "Python",
+    icon: `${icons_dir}/python.svg`,
+    color: "#ffdd55",
+  },
+  {
+    label: "C++",
+    icon: `${icons_dir}/cpp.svg`,
+    color: "#004482",
+  },
+  {
+    label: "Bash",
+    icon: `${icons_dir}/bash.svg`,
+    color: "#3e474a",
+  },
+  {
+    label: "Git",
+    icon: `${icons_dir}/git.svg`,
+    color: "#f05030",
+  },
+  {
+    label: "Figma",
+    icon: `${icons_dir}/figma.svg`,
+    color: "#a259ff",
+  },
+  {
+    label: "Linux",
+    icon: `${icons_dir}/linux.svg`,
+    color: "#ffcc00",
+  },
+];
