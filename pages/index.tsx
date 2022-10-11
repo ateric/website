8a1c4537cf555ce8790b858bac029ed628a11bb4
syntheit <daniel@matv.io>
2022-10-11 02:05:18 -0400

    Removed smooth corners and added link icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
-import SmoothCorner from "../components/SmoothCorner/SmoothCorner";
 import { navLinks } from "../constants/navigationLinks";
 import { index } from "../constants/metadata";
+import { links } from "../constants/links";
 
 const Home: NextPage = () => {
   return (
@@ -11,8 +12,8 @@ const Home: NextPage = () => {
     >
       <NextSeo {...index} />
       <div className="blurGradient"></div>
-      <SmoothCorner className="box-50 gradient_1 flex justify-center items-center movingBlurGradient z-10">
-        <SmoothCorner className="box-45 bg-black flex items-center justify-center flex-col z-0">
+      <div className="lg:box-50 rounded-2xl gradient_1 flex justify-center items-center movingBlurGradient z-10">
+        <div className="lg:box-45 rounded-2xl bg-black flex items-center justify-center flex-col z-0">
           <div className="flex justify-center flex-col">
             <h1 className="text-super-350 text-6xl font-bold">Daniel Miller</h1>
             <div className="w-10/12 h-2 mt-1 ml-1 bg-white rounded-full gradient_2"></div>
@@ -26,16 +27,29 @@ const Home: NextPage = () => {
                   <a
                     href={item.url}
                     key={item.name}
-                    className={`font-black italic text-7xl px-7 mx-10 mb-20 linkEffect linkEffect--${item.color}`}
+                    className={`flex justify-center items-center font-black italic text-7xl mx-10 h-40`}
                   >
-                    <h2>{item.name}</h2>
+                    <div
+                      className={`px-7 w-full linkEffect linkEffect--${item.color}`}
+                    >
+                      <h2 className="">{item.name}</h2>
+                    </div>
                   </a>
                 )
               )}
             </div>
           </div>
-        </SmoothCorner>
-      </SmoothCorner>
+          <div className="flex justify-center w-full">
+            {links.map(({ label, id, url, icon }) => (
+              <a key={id} href={url} title={label} className="mx-3">
+                <svg className="w-14 h-14 text-white linkIconHover">
+                  <use href={`${icon}#${id}`} />
+                </svg>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
