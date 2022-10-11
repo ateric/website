375149f634ec750925fc12531f4ddcdbd71e62ad
syntheit <daniel@matv.io>
2022-10-11 02:04:39 -0400

    Modified structure and added LinkedIn

--- constants/links.ts
+++ constants/links.ts
@@ -1,13 +1,21 @@
+const icons_dir: string = "/icons";
+
 export const links: {
   label: string;
+  id: string;
   url: string;
   icon: string;
-  hoverColor: string;
 }[] = [
   {
     label: "Github",
+    id: "github",
     url: "https://www.github.com/syntheit",
-    icon: "github.svg",
-    hoverColor: "#f5f5f5",
+    icon: `${icons_dir}/github.svg`,
+  },
+  {
+    label: "LinkedIn",
+    id: "linkedin",
+    url: "https://www.linkedin.com/in/daniel-miller-257658247/",
+    icon: `${icons_dir}/linkedin.svg`,
   },
 ];
