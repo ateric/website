1b69867b96ec455c2d78fa6f6f2ebd8e6f6b261c
syntheit <daniel@matv.io>
2022-11-15 23:19:32 -0500

    Added projects file

---
+++ constants/projects.ts
@@ -0,0 +1,68 @@
+import { Project } from "./types";
+
+export const projects: Project[] = [
+  {
+    name: "Soteria",
+    demo_url: "https://soteria.matv.io",
+    repo_url: "https://github.com/syntheit/soteria",
+    tools: ["React", "Next.js", "Firebase", "Tailwind", "Typescript", "Sass"],
+    description:
+      "Schools constantly throw out and destroy large amounts of technology.  Soteria is an app that connects faculty with students so that phased-out tech can be reused by students.",
+    start_date: "Nov 2022",
+    end_date: "Present",
+  },
+  {
+    name: "Binary Bit",
+    demo_url: "https://www.dev.binarybit.io",
+    repo_url: "https://github.com/BinaryBitLLC/binarybit/tree/develop",
+    tools: ["React", "Typescript", "Next.js", "Tailwind", "Sass", "Figma"],
+    description:
+      "Binary Bit is a open-source geared tech publication that I am currently working on.  The goal for Binary Bit is to be a place where users can learn about interesting tweaks or tools that can help their workflow.",
+    start_date: "Dec 2021",
+    end_date: "Present",
+  },
+  {
+    name: "Sailor",
+    repo_url: "https://github.com/syntheit/sailor",
+    tools: ["C++"],
+    description:
+      "Sailor is a program to manage containers using docker-compose files",
+    start_date: "Oct 2022",
+    end_date: "Present",
+  },
+  {
+    name: "Course Review",
+    tools: [
+      "React",
+      "Firebase",
+      "Javascript",
+      "Algolia",
+      "Express.js",
+      "styled-components",
+      "Figma",
+    ],
+    description:
+      "Course Review is a web app that allows students to make and view reviews for courses at their school.",
+    start_date: "Jun 2020",
+    end_date: "Dec 2020",
+  },
+  {
+    name: "Custom Shell Scripts",
+    repo_url: "https://github.com/syntheit/scripts",
+    tools: ["bash"],
+    description:
+      "A collection of utility scripts to automate tasks on my Linux systems.",
+    start_date: "Aug 2022",
+    end_date: "Present",
+  },
+  {
+    name: "Links",
+    demo_url: "https://www.links-demo.matv.io/",
+    repo_url: "https://github.com/syntheit/links",
+    tools: ["React", "Next.js", "Tailwind", "Sass"],
+    description:
+      "A simple site with buttons forwarding to different links.  Think of it as an open-source linktree.",
+    start_date: "Aug 2022",
+    end_date: "Aug 2022",
+  },
+];
